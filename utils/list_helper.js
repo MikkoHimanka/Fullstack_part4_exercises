edb406a8ebbe6f8dba476a9fd52d90d6582bdf8b
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-25 14:26:22 +0200

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	return blogs.map(x=>x.likes).reduce((a,b) => a + b, 0)
+}
+
+const favouriteBlog = (blogs) => {
+	const answer = blogs.reduce((a,b) => b.likes > a.likes ? b : a, blogs[0])
+	return {
+		title: answer.title,
+		author: answer.author,
+		likes: answer.likes
+	}
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes,
+	favouriteBlog
 }
