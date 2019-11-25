72db31a4eda93a099272ae67a703c45dd4629a2a
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-25 14:32:01 +0200

    4.5

--- tests/favouriteBlog.test.js
+++ tests/favouriteBlog.test.js
@@ -51,12 +51,30 @@ const bigList = [
     }  
 ]
 
+const oneList = [{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'Go To Statement Considered Harmful',
+  author: 'Edsger W. Dijkstra',
+  url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+  likes: 5,
+  __v: 0
+}]
 
-test('favourite blog is', () => {
-    const result = listHelper.favouriteBlog(bigList)
-    expect(result).toEqual({
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        likes: 12
+describe('favourite blog is', () => {
+  test('of a bigger list', () => {
+      const result = listHelper.favouriteBlog(bigList)
+      expect(result).toEqual({
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          likes: 12
+        })
+  })
+  test('of a list of one', () => {
+      const result = listHelper.favouriteBlog(oneList)
+      expect(result).toEqual({
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          likes: 5,
       })
+  })
 })
