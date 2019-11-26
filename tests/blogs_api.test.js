8c593ee0b782390412fc8dd71851c84523149074
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-26 14:57:05 +0200

    4.8

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('database has correct amount of blog entries', async () => {
+    const res = await api.get('/api/blogs')
+
+    expect(res.body.length).toBe(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
