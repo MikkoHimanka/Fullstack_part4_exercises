1de2c5471a69c78dc76396a03e7ced7a9e02d4f5
Mikko Himanka <mikko.himanka@protonmail.com>
2019-11-29 15:16:22 +0200

    4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -22,4 +22,29 @@ blogsRouter.post('/', async (request, response, next) => {
     }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception) 
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.json(updatedBlog.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
 module.exports = blogsRouter
